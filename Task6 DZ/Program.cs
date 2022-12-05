b22e24223c2fbfd1fdfb64d2d9fb5c8d3adff975
Dmitrii <petrovoo096@gmail.com>
2022-12-05 22:55:51 +0300

    jr

--- Task6 DZ/Program.cs
+++ Task6 DZ/Program.cs
@@ -3,3 +3,17 @@
 // a = 2 b = 10 -> max = 10, min = 2
 // a = -9 b = -3 -> max = -3, min = -9
 
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("{0} max", a);
+    Console.WriteLine("{0} min", b);
+}
+else 
+{
+    Console.WriteLine("{0} max", b);
+    Console.WriteLine("{0} min", a);
+}
